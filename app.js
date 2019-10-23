44d98f2f7a022db274938f079689e4cf73ecebb7
ddomagoj <ddespala@gmail.com>
2019-10-23 04:21:04 +0200

    New Commit

---
+++ app.js
@@ -0,0 +1,105 @@
+//BUDGET CONTROLLER
+var budgetController = (function() {
+  var Expense = function(id, description, value) {
+    this.id = id;
+    this.description = description;
+    this.value = value;
+  };
+
+  var Income = function(id, description, value) {
+    this.id = id;
+    this.description = description;
+    this.value = value;
+  };
+
+  var data = {
+    allItems: {
+      exp: [],
+      inc: []
+    },
+    totals: {
+      exp: 0,
+      inc: 0
+    }
+  };
+
+  return {
+    addItem: function(type, des, val) {
+      var newItem, ID;
+
+      if (data.allItems[type].lenght > 0) {
+        ID = data.allItems[type][data.allItems[type].lenght - 1].id + 1;
+      } else {
+        ID = 0;
+      }
+
+      if (type === "exp") {
+        newItem = new Expense(ID, des, val);
+      } else if (type === "inc") {
+        newItem = new Income(ID, des, val);
+      }
+
+      data.allItems[type].push(newItem);
+      return newItem;
+    },
+
+    testing: function() {
+      console.log(data);
+    }
+  };
+})();
+
+//UI CONTROLLER
+var UIController = (function() {
+  var DOMstrings = {
+    inputType: ".add__type",
+    inputDescription: ".add__description",
+    inputValue: ".add__value",
+    inputBtn: ".add__btn"
+  };
+
+  return {
+    getInput: function() {
+      return {
+        type: document.querySelector(DOMstrings.inputType).value,
+        description: document.querySelector(DOMstrings.inputDescription).value,
+        value: document.querySelector(DOMstrings.inputValue).value
+      };
+    },
+    getDOMstrings: function() {
+      return DOMstrings;
+    }
+  };
+})();
+
+//GLOBAL APP CONTROLLER
+var controller = (function(budgetCtrl, UICtrl) {
+  var setupEventListeners = function() {
+    var DOM = UICtrl.getDOMstrings();
+
+    document.querySelector(DOM.inputBtn).addEventListener("click", ctrlAddItem);
+
+    document.addEventListener("keypress", function(event) {
+      if (event.keyCode === 13 || event.which === 13) {
+        ctrlAddItem();
+      }
+    });
+  };
+  var ctrlAddItem = function() {
+    var input, newItem;
+    //get the field input data
+    input = UICtrl.getInput();
+
+    //add item to the budget controller
+    newItem = budgetCtrl.addItem(input.type, input.description, input.value);
+  };
+
+  return {
+    init: function() {
+      console.log("RADI");
+      setupEventListeners();
+    }
+  };
+})(budgetController, UIController);
+
+controller.init();
